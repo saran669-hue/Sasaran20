326fea660f3254b231ccc2d350d9505eab1510d2
saran669-hue <saranvutsaengaphai@gmail.com>
2025-03-18 06:42:09 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+//ฟังก์ชั่นการทำงานของปุ่มที่ 1
+function myFunction()
+    {
+        let x = document.getElementById("demo");
+            x.style.fontSize = "50px";
+            x.style.color = "green";
+    }
+//ฟังก์ชั่นการทำงานของปุ่มที่ 2
+function aleart()
+    {
+        alert("สวัดดีชาวโลก");
+        let Text = prompt("เจ้าเชื่อเรื่องสมิงบ่");
+        alert("อุอุ...อาอา " + Text + "!");
+    }
+
+//ฟังก์ชั่นการทำงานของปุ่มที่ 3
+function bt3()
+    {
+        document.getElementById('demo3').style.display='block';
+    }
